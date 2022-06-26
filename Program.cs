67825235ea3ede1f8dbb8be2561b72c3fbcc90a8
MarcelleTabosa <marcelle.tabosa.97@gmail.com>
2022-06-25 21:48:28 -0300

    Including delete, update and list users CRUD

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using supermercadosq.Entities;
 using supermercadosq.Model.Connection;
 
@@ -5,7 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddNpgsql<DatabaseConnection>(builder.Configuration["Database:Npgsql"]);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/user", (DatabaseConnection context) => {
+  List<User> user = context.Users.ToList();
+  return Results.Ok(user);;
+});
+
+app.MapGet("/user/single/{id}", ([FromRoute] int id, DatabaseConnection context) => {
+  var user = context.Users.Where(u => u.Id == id).First();
+  return Results.Ok(user);
+});
 
 app.MapPost("/user", (UserRequestDTO user, DatabaseConnection context) => {
   var u = new User{
@@ -22,6 +31,28 @@ app.MapPost("/user", (UserRequestDTO user, DatabaseConnection context) => {
   context.SaveChanges();
   return Results.StatusCode(201);
 });
+
+app.MapPut("/user/{id}", ([FromRoute]int id, UserRequestDTO userDTO, DatabaseConnection context) =>{
+  var user = context.Users.Where(u => u.Id == id).First();
+  if(user != null){
+    user.Name = userDTO.Name != null ? userDTO.Name : user.Name;
+    user.SocialName = userDTO.SocialName != null ? userDTO.SocialName : user.SocialName;
+    user.Email = userDTO.Email != null ? userDTO.Email : user.Email; 
+    user.Password = userDTO.Password != null ? userDTO.Password : user.Password;
+    user.PhoneNumber = userDTO.PhoneNumber != null ? userDTO.PhoneNumber : user.PhoneNumber;
+  }
+  context.SaveChanges();
+  return Results.StatusCode(204);
+});
+
+app.MapDelete("/user/{id}", ([FromRoute] int id, DatabaseConnection context) => {
+  var user = context.Users.Where(u => u.Id == id).First();
+  context.Users.Remove(user);
+  context.SaveChanges();  
+
+  return Results.StatusCode(204);
+});
+
 
 app.Run();
 
