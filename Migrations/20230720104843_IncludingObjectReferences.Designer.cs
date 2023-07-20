aa9dab25c90c060b0af728ca622f2a31d2310c8a
MarcelleTabosa <marcelle.tabosa.97@gmail.com>
2023-07-20 07:59:13 -0300

    including migrations and tool pack to automate their creation

--- Migrations/20230720104843_IncludingObjectReferences.Designer.cs
+++ Migrations/20230720104843_IncludingObjectReferences.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using supermercadosq.Data;
@@ -11,9 +12,10 @@ using supermercadosq.Data;
 namespace supermercadosq.Migrations
 {
     [DbContext(typeof(DatabaseConnection))]
-    partial class DatabaseConnectionModelSnapshot : ModelSnapshot
+    [Migration("20230720104843_IncludingObjectReferences")]
+    partial class IncludingObjectReferences
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
