7a403a40103f0054930e54f0b5ba15e474770e73
MarcelleTabosa <marcelle.tabosa.97@gmail.com>
2023-07-24 08:11:59 -0300

    implementing viacep integration

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using Refit;
 using supermercadosq.Data;
 using supermercadosq.Data.Interfaces;
 using supermercadosq.Data.Repository;
 using supermercadosq.Domain;
+using supermercadosq.Integration;
+using supermercadosq.Integration.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +17,12 @@ builder.Services.AddScoped<IRepository<Address>, AddressRepository>();
 builder.Services.AddScoped<IRepository<User>, UserRepository>();
 builder.Services.AddScoped<IRepository<Product>, ProductRepository>();
 builder.Services.AddScoped<IRepository<Comment>, CommentRepository>();
+builder.Services.AddScoped<IViaCepIntegration, ViaCepIntegration>();
+
+builder.Services.AddRefitClient<IViaCepIntegrationRefit>().ConfigureHttpClient(c =>
+{
+    c.BaseAddress = new Uri("https://viacep.com.br");
+});
 
 var app = builder.Build();
 
